12a9794d23862c0077e0ae35080f601bd84a21f9
Nandish N S <92267208+Nandish2021@users.noreply.github.com>
2022-12-28 09:46:36 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,7 @@ import navIcon3 from "../assets/img/nav-icon3.svg";
 
 export const Footer = () => {
   return (
+    <>
     <footer className="footer">
       <Container>
         <Row className="align-items-center">
@@ -50,5 +51,12 @@ export const Footer = () => {
         </Row>
       </Container>
     </footer>
+ {/* <!-- Scroll Top --> */}
+    <a href="#" aria-label="scroll-up" class="scrollup" id="scroll-up">
+    <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-arrow-up" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M8 15a.5.5 0 0 0 .5-.5V2.707l3.146 3.147a.5.5 0 0 0 .708-.708l-4-4a.5.5 0 0 0-.708 0l-4 4a.5.5 0 1 0 .708.708L7.5 2.707V14.5a.5.5 0 0 0 .5.5z"/>
+</svg>
+    </a>
+</>
   );
 };
